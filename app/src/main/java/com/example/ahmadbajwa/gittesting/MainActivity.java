2378911644531e3b3224666819cd471db4908c36
Ahmad Bajwa <ahmadanimom@gmail.com>
2017-10-03 23:59:19 +0500

    i am going to change again to test

---
+++ app/src/main/java/com/example/ahmadbajwa/gittesting/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.ahmadbajwa.gittesting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+   //super code goes here
+        public void view(){
+        //write coder here
+    }
+
+    }
+}
